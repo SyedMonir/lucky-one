6f5e27d21c1373d5129da3df6a985e9122b68c9d
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-03-26 07:20:13 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Syed Game Store </Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
